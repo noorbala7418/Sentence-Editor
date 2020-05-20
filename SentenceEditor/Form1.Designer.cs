5d8b87b805dd143706b907d2a67584a4933198f6
noorbala7418 <noorbala7418@gmail.com>
2020-05-20 16:25:06 +0430

    Version 1.0 . just converts Y in Persian To Y in Arabian.This App uses For Adobe Connect in order to Persian Typing.

---
+++ SentenceEditor/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace SentenceEditor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtEnter = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.resultLable = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtEnter
+            // 
+            this.txtEnter.Location = new System.Drawing.Point(52, 85);
+            this.txtEnter.Name = "txtEnter";
+            this.txtEnter.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.txtEnter.Size = new System.Drawing.Size(462, 96);
+            this.txtEnter.TabIndex = 0;
+            this.txtEnter.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label1.Location = new System.Drawing.Point(520, 99);
+            this.label1.Name = "label1";
+            this.label1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label1.Size = new System.Drawing.Size(52, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "متن شما:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label2.Location = new System.Drawing.Point(136, 20);
+            this.label2.Name = "label2";
+            this.label2.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label2.Size = new System.Drawing.Size(436, 40);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "راهنمایی: متن موردنظر خود را در باکس پایین نوشته و سپس دکمه ی تبدیل را بزنید\r\n تا" +
+    " به فرمت صحیح تبدیل شود.";
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.btnConvert.Location = new System.Drawing.Point(298, 226);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.btnConvert.Size = new System.Drawing.Size(89, 32);
+            this.btnConvert.TabIndex = 3;
+            this.btnConvert.Text = "تبدیل کن!";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // resultLable
+            // 
+            this.resultLable.AutoSize = true;
+            this.resultLable.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.resultLable.Location = new System.Drawing.Point(163, 283);
+            this.resultLable.Name = "resultLable";
+            this.resultLable.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.resultLable.Size = new System.Drawing.Size(0, 20);
+            this.resultLable.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 334);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(149, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Developed by : Noorbala7418";
+            // 
+            // btnReset
+            // 
+            this.btnReset.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.btnReset.Location = new System.Drawing.Point(192, 226);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.btnReset.Size = new System.Drawing.Size(91, 32);
+            this.btnReset.TabIndex = 6;
+            this.btnReset.Text = "پاکش کن";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label4.Location = new System.Drawing.Point(393, 327);
+            this.label4.Name = "label4";
+            this.label4.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label4.Size = new System.Drawing.Size(179, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "کار برنامه : تبدیل «ی» به «ي»";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(596, 359);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.resultLable);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtEnter);
+            this.Name = "Form1";
+            this.Text = "SentenceEditor";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox txtEnter;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.Label resultLable;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ SentenceEditor/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SentenceEditor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            String strtxtEnter =Convert.ToString(txtEnter.Text);
+            char[] chrTxtEnter = strtxtEnter.ToCharArray();
+
+            if (strtxtEnter.Equals("") == false)
+            {
+                for (int i = 0; i < chrTxtEnter.Length; i++)
+                {
+                    if (chrTxtEnter[i] == 'ی')
+                    {
+                        chrTxtEnter[i] = 'ي';
+                    }
+                }
+                strtxtEnter = new string(chrTxtEnter);
+                resultLable.Text = strtxtEnter.ToString();
+                Clipboard.SetText(strtxtEnter);
+                resultLable.Text = "در کلیپ بورد ذخیره شد. میتوانید پیست کنید";
+            }
+            else
+            {
+                resultLable.Text ="شما چیزی ننوشتید که بخوام تبدیل کنم";
+            }
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            txtEnter.Text = "";                
+        }
+    }
+}
