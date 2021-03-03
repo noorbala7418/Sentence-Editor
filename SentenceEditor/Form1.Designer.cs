340eee48dcad7c6dbb065fcd96721c7bbc8bb284
seyedali2561380 <seyedali2561380@gmail.com>
2021-03-04 00:37:01 +0330

    auto cop to clipboard- dark mode- high dpi optimization

--- SentenceEditor/Form1.Designer.cs
+++ SentenceEditor/Form1.Designer.cs
@@ -29,116 +29,152 @@
         private void InitializeComponent()
         {
             this.txtEnter = new System.Windows.Forms.RichTextBox();
-            this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.btnConvert = new System.Windows.Forms.Button();
             this.resultLable = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.btnReset = new System.Windows.Forms.Button();
             this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // txtEnter
             // 
-            this.txtEnter.Location = new System.Drawing.Point(52, 85);
+            this.txtEnter.Location = new System.Drawing.Point(39, 34);
+            this.txtEnter.Margin = new System.Windows.Forms.Padding(4);
             this.txtEnter.Name = "txtEnter";
             this.txtEnter.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.txtEnter.Size = new System.Drawing.Size(462, 96);
+            this.txtEnter.Size = new System.Drawing.Size(615, 117);
             this.txtEnter.TabIndex = 0;
             this.txtEnter.Text = "";
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
-            this.label1.Location = new System.Drawing.Point(520, 99);
-            this.label1.Name = "label1";
-            this.label1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.label1.Size = new System.Drawing.Size(52, 20);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "متن شما:";
+            this.txtEnter.TextChanged += new System.EventHandler(this.txtEnter_TextChanged);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
-            this.label2.Location = new System.Drawing.Point(136, 20);
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(69, 20);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
             this.label2.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.label2.Size = new System.Drawing.Size(436, 40);
+            this.label2.Size = new System.Drawing.Size(653, 50);
             this.label2.TabIndex = 2;
-            this.label2.Text = "راهنمایی: متن موردنظر خود را در باکس پایین نوشته و سپس دکمه ی تبدیل را بزنید\r\n تا" +
-    " به فرمت صحیح تبدیل شود.";
-            // 
-            // btnConvert
-            // 
-            this.btnConvert.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
-            this.btnConvert.Location = new System.Drawing.Point(298, 226);
-            this.btnConvert.Name = "btnConvert";
-            this.btnConvert.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.btnConvert.Size = new System.Drawing.Size(89, 32);
-            this.btnConvert.TabIndex = 3;
-            this.btnConvert.Text = "تبدیل کن!";
-            this.btnConvert.UseVisualStyleBackColor = true;
-            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            this.label2.Text = "راهنمایی: متن موردنظر خود را در باکس پایین بنویسید.\r\nمتن به صورت خودکار با فرمت ص" +
+    "حیح در کلیپ بورد ذخیره میشود و می توانید آنرا paste  کنید .";
             // 
             // resultLable
             // 
             this.resultLable.AutoSize = true;
             this.resultLable.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
-            this.resultLable.Location = new System.Drawing.Point(163, 283);
+            this.resultLable.Location = new System.Drawing.Point(217, 348);
+            this.resultLable.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.resultLable.Name = "resultLable";
             this.resultLable.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.resultLable.Size = new System.Drawing.Size(0, 20);
+            this.resultLable.Size = new System.Drawing.Size(0, 25);
             this.resultLable.TabIndex = 4;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(13, 334);
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(13, 410);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(149, 13);
+            this.label3.Size = new System.Drawing.Size(322, 17);
             this.label3.TabIndex = 5;
-            this.label3.Text = "Developed by : Noorbala7418";
+            this.label3.Text = "Developed by : Noorbala7418 && seyedali2561380";
             // 
             // btnReset
             // 
+            this.btnReset.BackColor = System.Drawing.Color.SpringGreen;
             this.btnReset.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
-            this.btnReset.Location = new System.Drawing.Point(192, 226);
+            this.btnReset.ForeColor = System.Drawing.Color.Black;
+            this.btnReset.Location = new System.Drawing.Point(313, 159);
+            this.btnReset.Margin = new System.Windows.Forms.Padding(4);
             this.btnReset.Name = "btnReset";
             this.btnReset.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.btnReset.Size = new System.Drawing.Size(91, 32);
+            this.btnReset.Size = new System.Drawing.Size(124, 49);
             this.btnReset.TabIndex = 6;
             this.btnReset.Text = "پاکش کن";
-            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.UseVisualStyleBackColor = false;
             this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
-            this.label4.Location = new System.Drawing.Point(393, 327);
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(524, 402);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
             this.label4.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.label4.Size = new System.Drawing.Size(179, 20);
+            this.label4.Size = new System.Drawing.Size(227, 25);
             this.label4.TabIndex = 7;
             this.label4.Text = "کار برنامه : تبدیل «ی» به «ي»";
             // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Controls.Add(this.btnReset);
+            this.groupBox1.Controls.Add(this.txtEnter);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.ForeColor = System.Drawing.Color.White;
+            this.groupBox1.Location = new System.Drawing.Point(17, 100);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.groupBox1.Size = new System.Drawing.Size(753, 221);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "sentence";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(662, 81);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label1.Size = new System.Drawing.Size(71, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "متن شما:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.ForeColor = System.Drawing.Color.White;
+            this.checkBox1.Location = new System.Drawing.Point(631, 177);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(105, 21);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.Text = "کیبورد لینوکسی ";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(596, 359);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(795, 442);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.btnReset);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.resultLable);
-            this.Controls.Add(this.btnConvert);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.txtEnter);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
             this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
             this.Text = "SentenceEditor";
+            this.TransparencyKey = System.Drawing.SystemColors.ActiveCaptionText;
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -147,13 +183,14 @@
         #endregion
 
         private System.Windows.Forms.RichTextBox txtEnter;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button btnConvert;
         private System.Windows.Forms.Label resultLable;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button btnReset;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox1;
     }
 }
 
